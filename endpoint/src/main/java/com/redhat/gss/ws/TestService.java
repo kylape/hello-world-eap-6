c0899ad6bf3adc3d1fa1976ad8c3a56e6dbaff34
Kyle Lape <kyle.lape@redhat.com>
2014-10-03 12:22:37 -0700

    First crack at log4j2

--- endpoint/src/main/java/com/redhat/gss/ws/TestService.java
+++ endpoint/src/main/java/com/redhat/gss/ws/TestService.java
@@ -9,22 +9,23 @@ import org.apache.cxf.annotations.EndpointProperty;
 import org.apache.cxf.annotations.Logging;
 import org.apache.cxf.interceptor.InInterceptors;
 
-import org.jboss.logging.Logger;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
 import org.jboss.ws.api.annotation.EndpointConfig;
 
 @WebService(endpointInterface="com.redhat.gss.ws.HelloWorld")
-@Logging(pretty=true)
+// @Logging(pretty=true)
 public class TestService implements HelloWorld {
-  private static Logger log = Logger.getLogger(TestService.class);
+  private static Logger log = LogManager.getLogger(TestService.class);
 
   public List<String> sayHello(List<String> names) throws Exception {
     List<String> greetings = new ArrayList<String>();
     for(String name : names) {
       greetings.add("Hello, " + name);
     }
-    if(log.isDebugEnabled()) {
+    if(log.isWarnEnabled()) {
       for(String greeting : greetings) {
-        log.debug(greeting);
+        log.warn(greeting);
       }
     }
     return greetings;
